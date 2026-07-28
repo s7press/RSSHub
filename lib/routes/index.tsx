4b7b1a6b675912f33e162ba4721f269124dbc810
s7press <rahim.rahimov84@gmail.com>
2026-07-28 12:40:23 +0400

    Simplify Home component to return 'Hello World'

--- lib/routes/index.tsx
+++ lib/routes/index.tsx
@@ -1,11 +1,4 @@
 // routes/index.tsx
-import React from "react";
-
 export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to S7Press</h1>
-      <p>This is the default homepage without APA changes.</p>
-    </div>
-  );
+  return <h1>Hello World</h1>;
 }
